6c8dba30a9d21689457c2d04ff34f2be06659a64
mbilalsheikh <nafeelaaqib@gmail.com>
2024-09-21 09:42:17 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'frontend')));
+
+app.use((req, res, next) => {
+    if (/(.ico|.js|.css|.jpg|.png|.map)$/i.test(req.path)) {
+        next();
+    } else {
+        res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+        res.header('Expires', '-1');
+        res.header('Pragma', 'no-cache');
+        res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
+    }
+
+    const data = res.json;
+
+});
+
+app.listen(4000, () => {
+    console.log("app started");
+    // console.log(Register_Route)
+})
